75efb6d2ed62b931c08fa0c1c544a276237054a7
Dreu <khadrygassama@gmail.com>
2015-10-14 17:30:01 +0200

    Premier commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Ares
+====
+
+A Symfony project created on October 14, 2015, 5:02 pm.
---
+++ src/Ares/SiteBundle/AresSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ares\SiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AresSiteBundle extends Bundle
+{
+}
