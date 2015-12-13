575f6d4d691047de45b096faa5a35323e955116c
Dreu <khadrygassama@gmail.com>
2015-12-13 21:09:21 +0100

    Creation des User

---
+++ src/Ares/UserBundle/AresUserBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ares\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AresUserBundle extends Bundle
+{
+    public function getParent()
+
+    {
+
+        return 'FOSUserBundle';
+
+    }
+}
---
+++ src/Ares/UserBundle/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Ares\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+}
+
